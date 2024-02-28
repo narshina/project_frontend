0309e85c92b63af21c13c9a362e8f0dd4374a710
narshina <narshina2001@gmail.com>
2024-02-28 22:45:38 +0530

    home modified

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import App from './App'
 import './Home.css';
-import img from './computer.png'
-import imgg from './icon.jpg'
 
 export default function Home() {
   return (
-    
-<>
-<div>
-   <div className='main'>
-    <div className=' one flex items-center pt-64 w-[510px] text-center text-[40px] pl-100'>
-      <img className='icon w-24 ' src={imgg} alt="" />
-      <div className='text-[40px] font-serif italic'>
-      GET YOUR SERVICES FROM HERE</div>
+    <>
+    <div className='bg-[#7E9BD5] h-[350px]'>
+      <div className='p-40 text-center'><label className='hover:text-white text-[#0F3053]  font-bold text-[40px]'>GET YOUR SERVICES FROM HERE</label><br></br><span className='text-[25px] text-center'>Empowering you with easy access to solutions.</span>
+      <div className='mt-[300px] h-64 w-72 bg-black'>
+        <h5>ABOUT US</h5>
       </div>
-   </div>
-</div>
-</>
+      </div>
+     
+    </div>
+    
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#7E9BD5" fill-opacity="1" d="M0,256L48,224C96,192,192,128,288,117.3C384,107,480,149,576,181.3C672,213,768,235,864,224C960,213,1056,171,1152,154.7C1248,139,1344,149,1392,154.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+      
+    </>
+    
+   
+
   )
 }
